93a564c0eed82dd854bc5e4f825ce4073e6f8781
LucyKanyua <lucykanyua@gmail.com>
2023-09-24 12:44:53 -0700

    added 100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# The below script sets up a client SSH configuration file so that you can connect to a server without typing a password.
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "Host *\nPasswordAuthentication no\nIdentityFile ~/.ssh/school\n"
+}
