dc7b3e1b16a8e9ecd453ac520ced564a4b680fb7
LucyKanyua <lucykanyua@gmail.com>
2023-10-23 11:30:26 -0700

    added 0-gather_data_from_an_API.py and README.md

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module uses the requests module to get the information about an employee's
+todo data
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        user_id = sys.argv[1]
+        user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+        user_data = requests.get(user_url).json()
+
+        todos_url = "https://jsonplaceholder.typicode.com/todos"
+        todos = requests.get(todos_url, params={'userId': user_id}).json()
+
+        completed_todos = list(filter(
+            lambda todo: todo.get("completed"), todos))
+
+        output = "Employee {} is done with tasks({}/{}):".format(
+                user_data.get("name"), len(completed_todos), len(todos)
+                )
+        print(output)
+        for todo in completed_todos:
+            print("\t {}".format(todo.get("title")))
