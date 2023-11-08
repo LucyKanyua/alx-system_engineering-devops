f839c40981d09c01a52e197bccdfcc6924ead12d
LucyKanyua <lucykanyua@gmail.com>
2023-11-08 10:54:56 -0800

    added README.md and 0-strace_is_your_friend.pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# The below script attempts to fix an Apache server that is returning a 500 error.
+
+exec {'fix wordpress config':
+  command => "/bin/sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+}
+
